6d7007cc7a88cd7b0c4908dd2c8f6d2b8c44359d
Mukhammadjon <muhammadjonhalimov22@gmail.com>
2022-09-15 16:07:34 +0500

    first commit, work.js

---
+++ work.js
@@ -0,0 +1,16 @@
+let count=0
+let count2=0
+
+for(m=0; m<=10; m++){
+    let qoldiq = m%2
+    if(qoldiq == 0){
+        console.log(m,"Juft")
+        count++
+    }
+    else{
+        console.log(m,"Toq")
+        count2++
+    }
+}
+console.log(count)
+console.log(count2)
